2a13a67aeeea7fb9ef199bb61ca1a5b3056341ea
Ojal <75155992+OjalKamble@users.noreply.github.com>
2021-05-25 15:51:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var balloon,balloonImage1,balloonImage2;
 var database,position;
 
 function preload(){
-   bg =loadImage("city image.jpg");
+   bg = loadImage("city image.jpg");
    balloonImage1=loadAnimation("hotairballoon1.png");
    balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
    "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
@@ -12,15 +12,16 @@ function preload(){
 
 //Function to set initial environment
 function setup() {
-  database=firebase.database();
+  database = firebase.database();
   createCanvas(1500,700);
 
-  balloon=createSprite(250,450,150,150);
+  balloon = createSprite(250,450,150,150);
   balloon.addAnimation("hotAirBalloon",balloonImage1);
-  balloon.scale= 1;
+  balloon.scale=0.5;
 
   var balloonPosition = database.ref('balloon/height');
-  balloonPosition.on("value",readHeight, showError);
+  balloonPosition.on("value",readPosition, showError);
+
 
   textSize(20); 
 }
@@ -32,41 +33,49 @@ function draw() {
   if(keyDown(LEFT_ARROW)){
     updateHeight(-10,0);
     balloon.addAnimation("hotAirBalloon",balloonImage2);
-  }
+    }
+
   else if(keyDown(RIGHT_ARROW)){
     updateHeight(10,0);
     balloon.addAnimation("hotAirBalloon",balloonImage2);
-  }
+    }
+
   else if(keyDown(UP_ARROW)){
     updateHeight(0,-10);
     balloon.addAnimation("hotAirBalloon",balloonImage2);
-    balloon.scale = balloon.scale -0.01;
-  }
+    balloon.scale=balloon.scale -0.01;
+    }
+
   else if(keyDown(DOWN_ARROW)){
-    updateHeight(0,10);
+    updateHeight(0,+10);
     balloon.addAnimation("hotAirBalloon",balloonImage2);
-  }
+    balloon.scale= balloon.scale +0.02;
+    }
+
 
   drawSprites();
   fill(0);
   stroke("white");
   textSize(25);
-  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+  text("*Use arrow keys to move Hot Air Balloon!",40,40);
 }
 
-function updateHeight(){
-  database.ref('balloon/height').set({
-    'x': height.x + x ,
-    'y': height.y + y
-  })
+function updateHeight(x,y){
+database.ref('balloon/height').set({
+  'x':height.x+x,
+  'y':height.y+y
+})
+
 }
 
-function readHeight(data){
+function readPosition(data){
   height = data.val();
   balloon.x = height.x;
   balloon.y = height.y;
+
+
 }
 
-function showError(){
+function  showError(){
   console.log("Error in writing to the database");
 }
